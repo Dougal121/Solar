de732fd2c1990115f84781ac2b9b1a3f29036033
Dougal Plummer <dougal@plummer.com.au>
2019-03-02 23:02:53 +1100

    Add files via upload

---
+++ LEDMATRIX.ino
@@ -0,0 +1,95 @@
+
+void DisplayMeatBall() {
+  int  pos , led , x , y;
+  float dx , dy ;
+  float dxa , dya ;
+
+  HT.setBrightness(15);
+
+  dx = xzAng - xzTarget ;
+  dy = yzAng - yzTarget ;
+  dxa = abs(dx) ;
+  dya = abs(dy) ;
+  if (dxa < 6) {
+    x = 0 ;
+  } else {
+    if (dxa < 12) {
+      x = sign(dx);
+    } else {
+      if (dxa < 50) {
+        x = 2 * sign(dx);
+      } else {
+        x = 3 * sign(dx);
+      }
+    }
+  }
+  if (dya < 6) {
+    y = 0 ;
+  } else {
+    if (dya < 12) {
+      y = sign(dy);
+    } else {
+      if (dya < 25) {
+        y = 2 * sign(dy);
+      } else {
+        y = 3 * sign(dy);
+      }
+    }
+  }
+  pos = 27 ; // netral position
+  pos += (y * 8) ; // add or sumtract the x in range of -3 to +3
+  pos += (x ) ; // add or sumtract 8 * y or y in range of -3 to +3
+  for (led = 0; led < 63; led++) {
+    switch (led){
+       case 0:
+       case 7:
+       case 56:
+       case 63:
+       break;
+       default:
+         HT.clearLed(MapLedNo(led));
+       break;  
+    }
+  }
+
+  HT.setLed(MapLedNo(0)); // turn on four courners
+  HT.setLed(MapLedNo(7));
+  HT.setLed(MapLedNo(56));
+  HT.setLed(MapLedNo(63));
+
+
+  //  HT.setLedNow(MapLedNo(tc.sec));
+  if ((iPWM_YZ == 0) && (iPWM_XZ == 0)) {
+    HT.setBlinkRate(HT16K33_DSP_NOBLINK); // not attempting to move
+    if (tc.sec % 2 == 0 ) {
+      HT.setLed(MapLedNo(pos + 0)); // display the meatball
+      HT.setLed(MapLedNo(pos + 9));
+    }else{
+      HT.setLed(MapLedNo(pos + 1));
+      HT.setLed(MapLedNo(pos + 8));
+    }
+  } else {
+    HT.setBlinkRate(HT16K33_DSP_BLINK2HZ); //moving so blink meat ball
+    HT.setLed(MapLedNo(pos + 0)); // display the meatball
+    HT.setLed(MapLedNo(pos + 1));
+    HT.setLed(MapLedNo(pos + 8));
+    HT.setLed(MapLedNo(pos + 9));
+  }
+  HT.sendLed() ; // update all in one go
+}
+
+
+int MapLedNo(int target)  // this compensates for the screwy setup of the matrix driver to the chip
+{
+  int row ;
+  int col ;
+  row = target / 8 ;
+  col = target % 8 ;
+  if (col == 0 ) {
+    return ((row * 16 ) + 7) ;
+  } else {
+    return ((row * 16 ) + col - 1) ;
+  }
+}
+
+
